2314e14df17ea5885f1704a45ae3034379dd07b1
Rose Momanyi <rosebmomanyi@gmail.com>
2025-04-24 00:00:47 +0300

    Styled my about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,35 @@
-// AboutPage.jsx
+import './About.css';
+
 export default function AboutPage() {
     return (
       <div>
-        <h1>About MedTrack</h1>
+        
+
+        <div className="about-page">
+  <section className="about-me">
+    <h2>About MedTrack</h2>
+    <p>
+      
+      After seeing how easily medication schedules can be forgotten or misunderstood,
+      I created this app to help patients track their doses and stay consistent with their treatments.
+    </p>
+  </section>
+
+  <section className="mission">
+    <h2>Mission</h2>
+    <p>
+      To support better health outcomes by making medication tracking simple, clear, and consistent for every patient.
+    </p>
+  </section>
+
+  <section className="vision">
+    <h2>Vision</h2>
+    <p>
+      A world where technology empowers patients to take control of their treatment with confidence and ease.
+    </p>
+  </section>
+</div>
+
         
       </div>
     );
